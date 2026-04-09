8cefc54213c20a622f2a6818ecf5ac4d39c88bb2
o-200 <omgout.200@gmail.com>
2026-04-09 14:56:51 +0700

    add redis

--- src/config/cache.config.ts
+++ src/config/cache.config.ts
@@ -1,6 +1,16 @@
+import KeyvRedis, { Keyv } from '@keyv/redis';
 import { ConfigService } from '@nestjs/config';
+import { CacheableMemory } from 'cacheable';
 
 export const cacheConfig = (configService: ConfigService) => ({
-  ttl: configService.getOrThrow<number>('CACHE_TTL'),
   isGlobal: configService.getOrThrow<boolean>('CACHE_IS_GLOBAL'),
+  stores: [
+    new Keyv({
+      store: new CacheableMemory({
+        ttl: configService.getOrThrow<string>('REDIS_TTL'),
+        lruSize: 5000,
+      }),
+    }),
+    new KeyvRedis(configService.getOrThrow<string>('REDIS_ADDRESS')),
+  ],
 });
