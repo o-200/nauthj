f71fc69cc9e0fb16d7a017ee4dc73f50a17cb325
o-200 <omgout.200@gmail.com>
2026-04-10 15:42:28 +0700

    fix providers

--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { usersProviders } from 'src/providers/databases/database_providers/user.providers';
+import { usersProviders } from 'src/providers/databases/database_providers/users.providers';
 import { DatabasesModule } from 'src/providers/databases/databases.module';
 
 @Module({
--- src/providers/databases/database_providers/avatars.providers.ts
+++ src/providers/databases/database_providers/avatars.providers.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { Avatar } from 'src/features/avatars/entities/avatar.entity';
 
-export const avatarProviders = [
+export const avatarsProviders = [
   {
     provide: 'AVATAR_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(Avatar),
