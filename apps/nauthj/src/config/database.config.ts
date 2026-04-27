df1092e75ccc43bc000a0b2b64bdf5ae271d869f
o-200 <omgout.200@gmail.com>
2026-04-27 11:48:03 +0700

    add comment

--- apps/nauthj/src/config/database.config.ts
+++ apps/nauthj/src/config/database.config.ts
@@ -17,7 +17,7 @@ export default registerAs(
     username: process.env[ENV_KEYS.POSTGRES_USER],
     password: process.env[ENV_KEYS.POSTGRES_PASSWORD],
     database: process.env[ENV_KEYS.POSTGRES_DB],
-    entities: [User, Avatar, Payment],
+    entities: [User, Avatar, Payment], // instead must use migrations
     synchronize: true,
     logging: ['query', 'error', 'schema', 'migration'],
   }),
